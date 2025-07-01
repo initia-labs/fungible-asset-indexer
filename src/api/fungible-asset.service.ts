987229a8899c674c4782b36f466d89ebdda1a2d8
hubur <hubur@asphodel.io>
2025-07-01 12:16:42 +0200

    🐛 Fix

--- src/api/fungible-asset.service.ts
+++ src/api/fungible-asset.service.ts
@@ -177,16 +177,16 @@ export class FungibleAssetService {
     if (!trackedDenoms.includes(denom)) {
       throw new HttpException('Untracked denom', 400);
     }
-    const CACHE_KEY = 'onyx-rewards';
+    const CACHE_KEY = `onyx-rewards-${denom}`;
 
     const cachedResult = this.cache.get<RewardsResponse>(CACHE_KEY);
     if (cachedResult) {
       return cachedResult;
     }
-
+    console.log(denom)
     let denominator = 0
     if (denom === 'move/07d1fef22a2d391eb47922148d58dc77db025f12d014ae98437a6fac051de4c4') denominator = 0.9
-    if (denom === '"move/35767058b322a9838d9bec0a7e96a19ab313a09d6b421bd6ee8ad675db7a687d') denominator = 0.1
+    if (denom === 'move/35767058b322a9838d9bec0a7e96a19ab313a09d6b421bd6ee8ad675db7a687d') denominator = 0.1
 
     const provider = new ethers.JsonRpcProvider('https://jsonrpc-yominet-1.anvil.asia-southeast.initia.xyz')
     const tokenAddress = '0x4badfb501ab304ff11217c44702bb9e9732e7cf4'
@@ -199,6 +199,7 @@ export class FungibleAssetService {
     try {
       const balance = await contract.balanceOf(walletAddress)
       const balanceToken = ethers.formatEther(balance) // Convert from wei to ether
+      console.log(`numba : ${(Number(balanceToken) / 14)} * ${denominator}`)
       const amount = (Number(balanceToken) / 14) * denominator
       
       const result = {
